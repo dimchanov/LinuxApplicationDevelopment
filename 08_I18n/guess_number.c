4ff335928a1b9249a62c772606982b57151b0ea5
dimchanov <dimchanov1121@mail.ru>
2024-11-08 17:36:24 +0300

    Guess number

---
+++ 08_I18n/guess_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    int left = 1, right = 100;
+    int middle;
+
+    while (left != right) {
+        middle = (left + right) / 2;
+        
+        printf("Is your number greater than %d?\n", middle);
+
+        char answer[10];
+        char is_correct_answer = 0;
+        while (!is_correct_answer) {
+            is_correct_answer = 1;
+            scanf("%s", answer);
+            
+            if (!strcmp(answer, "yes")) {
+                left = middle + 1;
+            } else if (!strcmp(answer, "no")) {
+                right = middle;
+            } else {
+                printf("Just answer the question yes or no, please\n");
+                is_correct_answer = 0;
+            }
+        }
+    }
+
+    printf("Your number is %d!\n", left);
+
+    return 0;
+}
