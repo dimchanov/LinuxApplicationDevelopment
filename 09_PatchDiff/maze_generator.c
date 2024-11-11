dcad19a8300917bb5e989aa64db774ab607da253
dimchanov <dimchanov1121@mail.ru>
2024-11-11 22:01:15 +0300

    Show impassable maze

--- 09_PatchDiff/maze_generator.c
+++ 09_PatchDiff/maze_generator.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 
 
+double p() {
+    return (double)rand() / RAND_MAX;
+}
+
+// void set_passage(char *maze, int size, int i, int j) {
+//     int offset = size * 2 + 1;
+//     maze[i * offset + j] = 1;
+//     // up
+//     if (p() > 0.5 && i - 2 > 0 && maze[(i - 2) * offset + j] != 1) {
+//         maze[(i - 1) * offset + j] = 1;
+//         set_passage(maze, size, i - 2, j);
+//     }
+//     // right
+//     if (p() > 0.5 && j + 2 < offset && maze[i * offset + j + 2] != 1) {
+//         maze[i * offset + j + 1] = 1;
+//         set_passage(maze, size, i, j + 2);
+//     }
+//     // down
+//     if (p() > 0.5 && i + 2 < offset && maze[(i + 2) * offset + j] != 1) {
+//         maze[(i + 1) * offset + j] = 1;
+//         set_passage(maze, size, i + 2, j);
+//     }
+// }
+
+
+void show_maze(char *maze, int size, char wall, char passage) {
+    int offset = size * 2 + 1;
+    for (int i = 0; i < offset; ++i) {
+        for (int j = 0; j < offset; ++j) {
+            printf("%c", maze[i * offset + j] ? passage : wall);
+        }
+        printf("\n");
+    }
+}
+
+
 int main(int argc, char **argv) {
     int size = 6;
     char wall = '#';
@@ -14,14 +50,9 @@ int main(int argc, char **argv) {
         for (int j = 1; j < offset; j += 2) {
             maze[i * offset + j] = 1;
         }
-    }    
-
-    for (int i = 0; i < offset; ++i) {
-        for (int j = 0; j < offset; ++j) {
-            printf("%c", maze[i * offset + j] ? passage : wall);
-        }
-        printf("\n");
     }
 
+    show_maze(maze, size, wall, passage);
+    
     return 0;
 }
