445d5ea2b24d6d98eebb02b6b6470d61a01977c3
dimchanov <dimchanov1121@mail.ru>
2024-11-13 17:45:19 +0300

    get open maze

--- 09_PatchDiff/maze_generator.c
+++ 09_PatchDiff/maze_generator.c
@@ -7,17 +7,36 @@ double p() {
 }
 
 
-void get_impassable_maze(char *maze, int size) {
+void get_open_maze(char *maze, int size) {
     int offset = size * 2 + 1;
     for (int i = 1; i < offset; i += 2) {
         for (int j = 1; j < offset; j += 2) {
-            maze[i * offset + j] = 1;
+            // up
+            if (i - 1 > 0) {
+                maze[(i - 1) * offset + j] = 1;
+                maze[i * offset + j] += 1;
+            }
+            // right
+            if (j + 1 < offset - 1) {
+                maze[i * offset + j + 1] = 1;
+                maze[i * offset + j] += 1;
+            }
+            // down
+            if (i + 1 < offset - 1) {
+                maze[(i + 1) * offset + j] = 1;
+                maze[i * offset + j] += 1;
+            }
+            // left
+            if (j - 1 > 0) {
+                maze[i * offset + j - 1] = 1;
+                maze[i * offset + j] += 1;
+            }
         }
     }
 }
 
 
-void set_passage(char *maze, int size, int i, int j) {
+void set_passage(char *maze, int size) {
     int offset = size * 2 + 1;
     for (int i = 1; i < offset; i += 2) {
         for (int j = 1; j < offset; j += 2) {
@@ -25,19 +44,19 @@ void set_passage(char *maze, int size, int i, int j) {
                 continue;
             }
             // up
-            if (p() > 0.5 && i - 2 > 0) {
+            if (p() > 0.75 && i - 2 > 0) {
                 maze[(i - 1) * offset + j] = 1;
             }
             // right
-            if (p() > 0.5 && j + 2 < offset) {
+            if (p() > 0.75 && j + 2 < offset) {
                 maze[i * offset + j + 1] = 1;
             }
             // down
-            if (p() > 0.5 && i + 2 < offset) {
+            if (p() > 0.75 && i + 2 < offset) {
                 maze[(i + 1) * offset + j] = 1;
             }
             // left
-            if (p() > 0.5 && j - 2 > 0) {
+            if (p() > 0.75 && j - 2 > 0) {
                 maze[i * offset + j - 1] = 1;
             }
         }
@@ -49,6 +68,7 @@ void show_maze(char *maze, int size, char wall, char passage) {
     int offset = size * 2 + 1;
     for (int i = 0; i < offset; ++i) {
         for (int j = 0; j < offset; ++j) {
+            // printf("%d ", maze[i * offset + j]);
             printf("%c", maze[i * offset + j] ? passage : wall);
         }
         printf("\n");
@@ -64,7 +84,9 @@ int main(int argc, char **argv) {
     int offset = size * 2 + 1;
     char *maze = (char *)calloc(offset * offset, sizeof(char));
 
-    get_impassable_maze(maze, size);
+    get_open_maze(maze, size);
+
+    // set_passage(maze, size);
 
     show_maze(maze, size, wall, passage);
     
