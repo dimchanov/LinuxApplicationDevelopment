64421d3bf0aefe6ae36eb4f5367c92db5f2e4556
dimchanov <dimchanov1121@mail.ru>
2024-11-30 12:29:13 +0300

    Add Function Documentation

--- 11_Documenting/guess_number.c
+++ 11_Documenting/guess_number.c
@@ -4,7 +4,13 @@
 #include <string.h>
 
 
+/**
+ *   Buffer for converting integer to a string
+ */
 char str_number[4] = {0};
+/**
+ *   A table for converting Arabic numbers from 1 to 100 to Roman numbers in string representation
+*/
 char* roman_numbers[100] = {
     "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
     "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
@@ -19,11 +25,20 @@ char* roman_numbers[100] = {
 };
 
 
+/** A table for converting Arabic numbers to Roman numbers in string representation
+ * @param number Arabic number 
+ * @return Roman number in string representation
+ */
 char* arabic2roman(int number) {
     return roman_numbers[number-1];
 }
 
 
+/** A table for converting Roman numbers in string representation to Arabic numbers
+ * @param roman Roman number in string representation
+ * @return integer Arabic number if Roman number from 1 to 100 
+ * else return -1
+ */
 int roman2arabic(char *roman) {
     for (int arabic = 0; arabic < 100; ++arabic) {
         if (!strcmp(roman, roman_numbers[arabic])) {
@@ -34,12 +49,20 @@ int roman2arabic(char *roman) {
 }
 
 
+/** A table for converting Arabic numbers to string
+ * @param number Arabic number 
+ * @return number in string representation
+ */
 char* number2string(int number) {
     sprintf(str_number, "%d", number);
     return str_number;
 }
 
-
+/** Main
+ * @param argc a counter for the number of command line arguments passed to the program.
+ * @param argv  pointer to an array of strings that contains program arguments.
+ * @return 0 if program have executed without errors else other code indicates an error is encountered.
+ */
 int main(int argc, char **argv) {
     setlocale(LC_ALL, "");
     bindtextdomain("guess_number", ".");
